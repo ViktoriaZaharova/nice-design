422fb93e14ffa599f545777e600ee4eaa4ec82c6
Viktoria <zaharova181114@gmail.com>
2021-01-16 12:07:52 +0200

    mobile menu

--- app/js/common.js
+++ app/js/common.js
@@ -7,3 +7,11 @@ $('.case-slider').slick({
 });
 
 $('[name="phone"]').mask('+7(999) 999-99-99');
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
